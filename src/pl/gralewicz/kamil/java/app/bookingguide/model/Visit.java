eb78ec71b86af54918b91881f1ddae369c0eded2
Kamil Gralewicz <k.gralewicz@gmail.com>
2023-06-07 15:55:56 +0200

    Added attributes for Visit.

--- src/pl/gralewicz/kamil/java/app/bookingguide/model/Visit.java
+++ src/pl/gralewicz/kamil/java/app/bookingguide/model/Visit.java
@@ -1,4 +1,31 @@
 package pl.gralewicz.kamil.java.app.bookingguide.model;
 
+import java.time.LocalDateTime;
+
 public class Visit {
+
+    private Client client;
+    private Service service;
+    private Shop shop;
+
+    private LocalDateTime dueDate;
+
+    public Visit(Client client, Service service, Shop shop, LocalDateTime dueDate) {
+        this.client = client;
+        this.service = service;
+        this.shop = shop;
+        this.dueDate = dueDate;
+    }
+
+    // TODO: dodać metody do obsługi wizyty (zaproponować w formie komentarza)
+
+    @Override
+    public String toString() {
+        return "Visit{" +
+                "client=" + client +
+                ", service=" + service +
+                ", shop=" + shop +
+                ", dueDate=" + dueDate +
+                '}';
+    }
 }
