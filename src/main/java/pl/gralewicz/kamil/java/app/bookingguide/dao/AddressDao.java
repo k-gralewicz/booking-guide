b79a51992cdd5b21f123e6e5775f5a0544a47725
Kamil Gralewicz <k.gralewicz@gmail.com>
2023-08-23 11:07:14 +0200

    Added JDBC implementation for create along with test.

--- src/main/java/pl/gralewicz/kamil/java/app/bookingguide/dao/AddressDao.java
+++ src/main/java/pl/gralewicz/kamil/java/app/bookingguide/dao/AddressDao.java
@@ -3,6 +3,10 @@ package pl.gralewicz.kamil.java.app.bookingguide.dao;
 import pl.gralewicz.kamil.java.app.bookingguide.controller.model.Address;
 import pl.gralewicz.kamil.java.app.bookingguide.controller.model.Shop;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -15,6 +19,21 @@ public class AddressDao {
         // TODO: 09.08.2023 dodać test jednostkowy dla metody create w AddressDao
         // stworzyć tabelę ADDRESS, która ma kolumny takie jak pola w klasie Address.
         // zaimplementować metodę create analogicznie do CarJdbcDao z pojektu java-learn.
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO ADDRESS( ID , STREET , FLAT_NUMBER , POST_CODE , CITY , COUNTRY ) VALUES(?, ?, ?, ?, ?, ?);");
+            preparedStatement.setInt(1, RandomIdGenerator.generate());
+            preparedStatement.setString(2, address.getStreet());
+            preparedStatement.setString(3, address.getFlatNumber());
+            preparedStatement.setString(4, address.getPostCode());
+            preparedStatement.setString(5, address.getCity());
+            preparedStatement.setString(6, address.getCountry());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         LOGGER.info("create(...)=");
         return null;
     }
