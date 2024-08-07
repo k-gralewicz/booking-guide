cb7d0cd81b9ec0651a6cd0524aa89b0b8c8b5883
Kamil Gralewicz <k.gralewicz@gmail.com>
2024-08-07 17:20:25 +0200

    Added dependencies and Application class.

---
+++ src/main/java/pl/gralewicz/kamil/java/app/bookingguide/BookingGuideApplication.java
@@ -0,0 +1,11 @@
+package pl.gralewicz.kamil.java.app.bookingguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingGuideApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookingGuideApplication.class,args);
+    }
+}
