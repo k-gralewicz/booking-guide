b92c3484aa2fd59fdf777e670af6d783b7b8247d
Kamil Gralewicz <k.gralewicz@gmail.com>
2024-08-21 17:19:53 +0200

    Added http request and dashboard.

--- src/main/java/pl/gralewicz/kamil/java/app/bookingguide/controller/ShopController.java
+++ src/main/java/pl/gralewicz/kamil/java/app/bookingguide/controller/ShopController.java
@@ -1,4 +1,23 @@
 package pl.gralewicz.kamil.java.app.bookingguide.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.logging.Logger;
+
+@Controller
+@RequestMapping(value = "/shops")
 public class ShopController {
+    private static Logger LOGGER = Logger.getLogger(ShopController.class.getName());
+
+    @GetMapping
+    public String method(String name) {
+        LOGGER.info("method(" + name + ")");
+
+        LOGGER.info("method(...)= ");
+        return "shop-dashboard";
+    }
 }
+
+// TODO: 21.08.2024 co to są servlety, przeczytać, nauczyć się
