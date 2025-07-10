208208b0bd2b71d0a5f029a0e7c953f96cce87fe
Kamil Gralewicz <k.gralewicz@gmail.com>
2025-07-10 18:35:25 +0200

    Added ID download by JQuery library.

--- src/main/resources/static/js/client-dashboard.js
+++ src/main/resources/static/js/client-dashboard.js
@@ -1,15 +1,16 @@
 function selectShop() {
-//    alert("Wybrano sklep JS");
+    //    alert("Wybrano sklep JS");
 
-    const params = new URLSearchParams({ username: 'Kamil'});
-        const url = '/visits/create?' + params;
+    const params = new URLSearchParams({ username: 'Kamil' });
+    const url = '/visits/create?' + params;
     fetch(url)
         // .then(response => response.json())
         .then(data => {
             // handle the response data
-            console.log(data);
-                // reload the page with the fetch URL
-                window.location.href = url;
+            var shopValue = $('#shop').val();
+            console.log(shopValue);
+            // reload the page with the fetch URL
+            window.location.href = url;
 
         })
         .catch(error => {
